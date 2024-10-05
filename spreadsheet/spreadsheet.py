ec499f9670a7a8e8b680cf4ab44f718ae2035ffb
Noel Yap <noel.yap+github.com@gmail.com>
2024-10-04 18:09:15 -0700

    Efficiency improved by having sheet keep track of topologically sorted list of cells

--- spreadsheet/spreadsheet.py
+++ spreadsheet/spreadsheet.py
@@ -18,6 +18,27 @@ class Cell:
         self.expr = lambda _: 0
         self.val = 0
 
+    @staticmethod
+    def topologically_sorted(cells: List['Cell']):
+        result = []
+
+        in_degree = {c: len(c.dependencies) for c in cells}
+
+        queue = [c for c in in_degree if in_degree[c] == 0]
+        while queue:
+            cell = queue.pop(0)
+            result.append(cell)
+
+            for d in cell.dependents:
+                in_degree[d] -= 1
+                if in_degree[d] == 0:
+                    queue.append(d)
+
+        if len(result) != len(in_degree):
+            raise ValueError("Cyclic reference detected")
+
+        return result
+
     def set_expr(self, expr: Expr):
         self.expr = expr
 
@@ -38,48 +59,28 @@ class Cell:
         return self.val
 
     def update_val(self, sheet: 'Sheet'):
-        for c in self.topologically_sorted_dependents():
-            c.val = c.expr(sheet)
-
-    def topologically_sorted_dependents(self) -> Optional[List['Cell']]:
-        result = []
-
-        processed = set()
-        dependencies: Dict[Cell, Set[Cell]] = defaultdict(Set[Cell])
-        dependencies[self] = set()
-        queue = [self]
-        while queue:
-            cell = queue.pop()
-            if cell not in processed:
-                processed.add(cell)
-                queue.extend(cell.dependents)
-                for d in cell.dependents:
-                    dependencies[d] = dependencies.get(d, set())
-                    dependencies[d].add(cell)
-
-        in_degree = {c: len(d) for c, d in dependencies.items()}
-
+        # TODO(nyap): consider caching the subgraph to improve compute efficiency (at the expense of memory efficiency)
+        #             this would require updating the cache each time the subgraph is updated
+        subgraph = []
         queue = [self]
         while queue:
             cell = queue.pop(0)
-            result.append(cell)
+            if cell not in subgraph:
+                subgraph.append(cell)
+                queue.extend(cell.dependents)
 
-            for d in cell.dependents:
-                in_degree[d] -= 1
-                if in_degree[d] == 0:
-                    queue.append(d)
-
-        if len(result) != len(in_degree):
-            raise ValueError("Cyclic reference detected")
-
-        return result
+        for c in sheet.topologically_sorted_cells:
+            if c in subgraph:
+                c.val = c.expr(sheet)
 
 
 class Sheet:
     cells: defaultdict[str, defaultdict[int, Cell]]
+    topologically_sorted_cells: List[Cell]
 
     def __init__(self):
         self.cells = defaultdict(lambda: defaultdict(Cell))
+        self.topologically_sorted_cells = []
 
     def set_contents(self, addr: str, contents: str):
         expr, addr_refs = self.convert_contents_to_callable(contents)
@@ -87,6 +88,16 @@ class Sheet:
         cell = self.get_cell(Addr(addr))
         cell.set_expr(expr)
         cell.set_dependencies(set(self.get_cell(addr) for addr in addr_refs))
+
+        try:
+            cells_to_be_topologically_sorted = set(self.topologically_sorted_cells)
+            cells_to_be_topologically_sorted.add(cell)
+            cells_to_be_topologically_sorted.update(cell.dependencies)
+            self.topologically_sorted_cells = Cell.topologically_sorted(list(cells_to_be_topologically_sorted))
+        except ValueError:
+            # FIXME(nyap): figure out what to do when there's a cyclic reference
+            pass
+
         cell.update_val(self)
 
     def get_cell(self, addr: Addr) -> Cell:
--- spreadsheet/spreadsheet_test.py
+++ spreadsheet/spreadsheet_test.py
@@ -21,6 +21,45 @@ class TestConvertContentsToCallable(unittest.TestCase):
 
 
 class TestCell(unittest.TestCase):
+    def test_topologically_sorted_dependents(self):
+        a1 = Cell()
+        a2 = Cell()
+        b1 = Cell()
+        b1.set_dependencies({a1, a2})
+        b2 = Cell()
+        b2.set_dependencies({a1, a2})
+        c1 = Cell()
+        c1.set_dependencies({a1, b1, b2})
+        c2 = Cell()
+        c2.set_dependencies({b1, b2})
+        d1 = Cell()
+        d1.set_dependencies({b1, c1})
+
+        actual = Cell.topologically_sorted([d1, c2, c1, b2, b1, a2, a1])
+
+        self.assertLess(actual.index(a1), actual.index(b1))
+        self.assertLess(actual.index(a1), actual.index(b2))
+        self.assertLess(actual.index(a1), actual.index(c1))
+        self.assertLess(actual.index(b1), actual.index(c1))
+        self.assertLess(actual.index(b1), actual.index(c2))
+        self.assertLess(actual.index(b1), actual.index(d1))
+        self.assertLess(actual.index(b2), actual.index(c1))
+        self.assertLess(actual.index(b2), actual.index(c2))
+        self.assertLess(actual.index(c1), actual.index(d1))
+
+    def test_cyclic_reference_detection(self):
+        a1 = Cell()
+        a2 = Cell()
+        a3 = Cell()
+        a1.set_dependencies({a2})
+        a2.set_dependencies({a3})
+        a3.set_dependencies({a1})
+
+        with self.assertRaises(ValueError) as ctx:
+            Cell.topologically_sorted([a1, a2, a3])
+
+        self.assertEqual(str(ctx.exception), "Cyclic reference detected")
+
     def test_set_dependencies(self):
         a1 = Cell()
         a2 = Cell()
@@ -46,53 +85,78 @@ class TestCell(unittest.TestCase):
         a1.expr = lambda _: 2
         a2 = Cell()
         a2.expr = lambda _: 3
+        a3 = Cell()
+        a3.expr = lambda _: 5
 
         a1.dependents = {a2}
+        a2.dependents = {a3}
 
         self.assertEqual(0, a1.val)
         self.assertEqual(0, a2.val)
+        self.assertEqual(0, a3.val)
 
-        a1.update_val(Sheet())
+        sheet = Sheet()
+        sheet.topologically_sorted_cells = [a1, a2, a3]
+
+        a1.update_val(sheet)
 
         self.assertEqual(2, a1.val)
         self.assertEqual(3, a2.val)
+        self.assertEqual(5, a3.val)
+
+    def test_update_subgraph(self):
+        a1 = Cell()
+        a1.expr = lambda _: 2
+        a2 = Cell()
+        a2.expr = lambda _: 3
+        a3 = Cell()
+        a3.expr = lambda _: 5
+
+        a1.dependents = {a2}
+        a2.dependents = {a3}
+
+        self.assertEqual(0, a1.val)
+        self.assertEqual(0, a2.val)
+        self.assertEqual(0, a3.val)
+
+        sheet = Sheet()
+        sheet.topologically_sorted_cells = [a1, a2, a3]
+
+        a2.update_val(sheet)
+
+        self.assertEqual(0, a1.val)
+        self.assertEqual(3, a2.val)
+        self.assertEqual(5, a3.val)
 
 
 class TestSheet(unittest.TestCase):
-    def test_topologically_sorted_dependents(self):
+    def test_set_contents(self):
         sheet = Sheet()
-        sheet.set_contents("D1", "=B1+C1")
-        sheet.set_contents("C1", "=A1+B1+B2")
-        sheet.set_contents("C2", "=B1+B2")
-        sheet.set_contents("B1", "=A1+A2")
-        sheet.set_contents("B2", "=A1+A2")
+        sheet.set_contents("A1", "2")
+        sheet.set_contents("A2", "=A1")
 
-        a1 = sheet.get_cell(Addr("A1"))
-        b1 = sheet.get_cell(Addr("B1"))
-        b2 = sheet.get_cell(Addr("B2"))
-        c1 = sheet.get_cell(Addr("C1"))
-        c2 = sheet.get_cell(Addr("C2"))
-        d1 = sheet.get_cell(Addr("D1"))
+        self.assertEqual(2, sheet.get_val("A1"))
+        self.assertEqual(2, sheet.get_val("A2"))
+        self.assertEqual(
+            [
+                sheet.get_cell(Addr("A1")),
+                sheet.get_cell(Addr("A2"))
+            ],
+            sheet.topologically_sorted_cells)
 
-        actual = a1.topologically_sorted_dependents()
+    def test_set_contents_reverse(self):
+        sheet = Sheet()
+        sheet.set_contents("A2", "=A1")
+        sheet.set_contents("A1", "2")
 
-        self.assertLess(actual.index(a1), actual.index(b1))
-        self.assertLess(actual.index(a1), actual.index(b2))
-        self.assertLess(actual.index(a1), actual.index(c1))
-        self.assertLess(actual.index(b1), actual.index(c1))
-        self.assertLess(actual.index(b1), actual.index(c2))
-        self.assertLess(actual.index(b1), actual.index(d1))
-        self.assertLess(actual.index(b2), actual.index(c1))
-        self.assertLess(actual.index(b2), actual.index(c2))
-        self.assertLess(actual.index(c1), actual.index(d1))
-
-    def test_cyclic_reference(self):
-        with self.assertRaises(ValueError) as ctx:
-            sheet = Sheet()
-            sheet.set_contents("A1", "=A2")
-            sheet.set_contents("A2", "=A1")
-
-        self.assertEqual(str(ctx.exception), "Cyclic reference detected")
+        self.assertEqual(2, sheet.get_val("A1"))
+        self.assertEqual(2, sheet.get_val("A2"))
+        self.assertEqual(
+            [
+                sheet.get_cell(Addr("A1")),
+                sheet.get_cell(Addr("A2"))
+            ],
+            sheet.topologically_sorted_cells)
 
 
 if __name__ == '__main__':
